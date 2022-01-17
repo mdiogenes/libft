9669b152d3ce9be53d0a8b59f9b31729073e9d1e
mdiogenes <mdiogenes@gmail.com>
2022-01-17 11:59:57 +0100

    ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,14 @@
+
+#include "libft.h"
+char *ft_substr(const char  *s, unsigned int start, size_t len)
+{
+	char	*dst; 
+	int	lens;
+	
+
+	dst = (char *)malloc(sizeof(const char) * (len));
+	if (dst == 0)
+		return (0);
+	lens = ft_strlcpy(dst, s, len);
+	return (dst);
+}
