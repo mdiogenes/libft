ca15cd864013575749ec7761616e9395f10f587e
mdiogenes <mdiogenes@gmail.com>
2022-01-17 12:39:32 +0100

    substr reparat i strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,19 @@
+
+#include "libft.h"
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*dst;
+	unsigned int	sizes1;
+	unsigned int	sizes2;
+	int		lendst;
+
+	sizes1 = ft_strlen((char *)s1);
+	sizes2 = ft_strlen((char *)s2);
+	dst = (char *)malloc(sizeof (const char)*(sizes1 + sizes2));
+	if (dst == 0)
+		return(0);
+	lendst = ft_strlcat(dst, s1, (sizes1));
+	lendst = ft_strlcat(dst, s2, (sizes1 + sizes2));
+	return (dst);	
+}
--- ft_substr.c
+++ ft_substr.c
@@ -2,10 +2,10 @@
 #include "libft.h"
 char *ft_substr(const char  *s, unsigned int start, size_t len)
 {
-	char	*dst; 
-	int	lens;
+	char		*dst; 
+	unsigned int	lens;
 	
-
+	dst = (char *)&start;
 	dst = (char *)malloc(sizeof(const char) * (len));
 	if (dst == 0)
 		return (0);
