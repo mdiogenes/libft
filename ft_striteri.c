6c9102879f81642f9fff9b95743244f69679a169
mdiogenes <mdiogenes@gmail.com>
2022-01-18 11:22:09 +0100

    itoa,strmapi,striteri reparats

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,23 +1,14 @@
 
 #include "libft.h"
 
-/*This function applies the parameter function f to each character of a string
- * also passed in it's parameter at at precisely that character's index string
- * position. Each character that is passed into the function f is modified if
- * necessary.*/
-
 void		ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	/*We start by creating our counter variable i as an unsigned variable to
-	 * compensate for the possibilty of a long string.*/
 	unsigned int	i;
 
-	/*We then start at the beginning of our string and work our way until the
-	 * end, applying the function f to each character. When our loop reaches
-	 * the of the string the function is finished.*/
 	i = 0;
 	while (s[i] != '\0')
 	{
+		//igual que strmapi pero passem la direccio en comptes del punter
 		f(i, s + i);
 		i++;
 	}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,19 +1,17 @@
 #include "libft.h"
 
-	/*This function is exactly the same as ft_strmap except that when we run
-	 * our given function f on the character we also pass to f the specific
-	 * index of our character in the given string. Review ft_strmap in order
-	 * to understand how this function works.*/
-
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*str;
 
 	i = 0;
+	//reservem lespai de la cadena a tornar amb el +1 pel \0
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
-	if (str == NULL)
-		return (NULL);
+	//control errors malloc
+	if (str == 0)
+		return (0);
+	//per cada caracter del string que ens passen i apliquem la funcio passantli el seu index
 	while (s[i] != '\0')
 	{
 		str[i] = f(i, s[i]);
