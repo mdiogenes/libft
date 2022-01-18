cd9f98d86cca8b264d35539ab33c04bc102b22fd
mdiogenes <mdiogenes@gmail.com>
2022-01-18 12:27:50 +0100

    split reparat

--- ft_split.c
+++ ft_split.c
@@ -1,28 +1,67 @@
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+#include "libft.h"
+
+static int	count_strings(const char *str, char c)
 {
-	char	*s2;
-	char	*s1;
-	char	**dst;
-	int	lens2;
-	int	lens1;
-		
-	lens1 = ft_strlen(s);
-	s1 = (char *)malloc((sizeof int) * lens1);
-	s2 = (char *)malloc((sizeof int) * lens1);
-	dst =(char *)malloc(sizeof *);
-	if (!s1 || !s2 || !dst)
-		return (0);
-	s2 = ft_strchr(s, (int *)c);
-	lens2 = ft_strlen(s2);
-	dst[1] = ft_strcpy(dst[1], s2);
-	//el strcopy no se si copia el \0
-	dst[1][lens2 + 1] = '\0'; 
-	lens1 = lens1 - lens2 ;
-	dst[0] = s1;
-	dst[0][lens1 + 1] = '\0';
-	free (s2)
-	return (dst);
+	int x;
+	int found;
+	//contem amb quantes strings hem de separar mirant
+	x = 0;
+	found = 0;
+	while (*str)
+	{
+		if (*str != c && found == 0)
+		{
+			found = 1;
+			x++;
+		}
+		else if (*str == c)
+			found = 0;
+		str++;
+	}
+	return (x);
 }
+
+static char	*word_dup(const char *str, int start, int finish)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	word = malloc((finish - start + 1) * sizeof(char));
+	while (start < finish)
+		word[i++] = str[start++];
+	word[i] = '\0';
+	return (word);
+}
+
+char		**ft_split(char const *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	int		index;
+	char	**split;
+	//contem amb quantes strings hem de separar la string s
+	if (!s || !(split = malloc((count_strings(s, c) + 1) * sizeof(char *))))
+		return (0);
+	i = 0;
+	j = 0;
+	index = -1;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		//ens afegeix tota la string separada pel delimitador
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			split[j++] = word_dup(s, index, i);
+			index = -1;
+		}
+		i++;
+	}
+	split[j] = "\0";
+	return (split);
+}
+
