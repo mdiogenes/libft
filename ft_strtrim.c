075cec159efc372df365d7a4522d769c2b19ce3a
mdiogenes <mdiogenes@gmail.com>
2022-01-18 18:44:35 +0100

    strtrim2

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -4,25 +4,25 @@
 char *ft_strtrim(char const *s1, char const *set)
 {
 	char	*dst;
-	int	c;
 	int	len;
+	int	x;
 
 	if (!s1 || !set)
 		return (0);
-	c = (int *) *set;
 	len = ft_strlen(s1);
-	dst = (char *) malloc (sizeof char * (len + 1);
+	dst = (char *) malloc (sizeof char * (len + 1));
 	if (!dst)
 		return (0);
 	ft_strlcpy(dst, s1, len);
-
-	while ((*set != '\0') && ( ft_strchr(dst, c)))
-	{
-		dst ++;
-	}
-	while ((len > 0) && (ft_strchr(dst[len], c)))
+	i = 0
+	while ((*(set + i) != '\0') && ( ft_strchr(&(dst + i), ((int *) *(set + i)))))
+		i ++;
+	i = 0;
+	while ((len > 0) && (ft_strchr(&(dst + len), ((int *) *(set + i)))))
 	{
 		len --;
+		i ++,
 	}
 	dst[len + 1] = '\0';
+	return (dst);
 }
